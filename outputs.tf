531267b189cb47584339a6f31ababd54f17246d2
Toshiya Watanabe <tw021955@gmail.com>
2025-11-01 00:20:19 +0900

    [Change]outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -4,3 +4,13 @@
 output "alb_dns_name" {
   value = module.alb.alb_dns_name
 }
+
+output "bastion_public_ip" {
+  description = "Bastion server public IPv4"
+  value       = aws_instance.bastion.public_ip
+}
+
+output "bastion_public_dns" {
+  description = "Bastion public DNS"
+  value       = aws_instance.bastion.public_dns
+}
