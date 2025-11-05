5d22d0111252dca8557bec6f9a3affbf2afc3c01
Toshiya Watanabe <tw021955@gmail.com>
2025-11-05 15:11:47 +0900

    [Change]variables

--- modules/security/variables.tf
+++ modules/security/variables.tf
@@ -1,7 +1,23 @@
-variable "vpc_id" { type = string }
+variable "vpc_id" {
+  type = string
+}
 
-variable "ec2_sg_name"     { type = string, default = "ec2" }
-variable "ansible_sg_name" { type = string, default = "ansible" }
+variable "ec2_sg_name" {
+  type    = string
+  default = "ec2"
+}
 
-variable "create_alb_sg" { type = bool, default = true }
-variable "alb_sg_name"   { type = string, default = "alb" }
+variable "ansible_sg_name" {
+  type    = string
+  default = "ansible"
+}
+
+variable "create_alb_sg" {
+  type    = bool
+  default = true
+}
+
+variable "alb_sg_name" {
+  type    = string
+  default = "alb"
+}
