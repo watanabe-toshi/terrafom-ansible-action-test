9372a4f72a1b08100c938a2dff64deae2b7eaec3
Toshiya Watanabe <tw021955@gmail.com>
2025-11-05 15:46:50 +0900

    Del

--- outputs.tf
+++ outputs.tf
@@ -6,8 +6,3 @@ output "ansible_public_ip" {
   description = "ansible public IPv4"
   value       = module.instances.ansible_public_ip
 }
-
-output "ansible_public_dns" {
-  description = "ansible public DNS"
-  value       = module.instances.ansible_public_dns
-}
